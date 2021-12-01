f9705af6fd010ece5ea529232dd3a886f293a8b4
Joachim Holwech <joachim.holwech@gmail.com>
2021-12-01 16:14:55 +0100

    Add all relevant files

---
+++ src/hello_world.py
@@ -0,0 +1,10 @@
+"""This file is an example containing a docstring.
+"""
+
+def say_hello():
+    """This function return a string containing "Hello world!".
+
+    Returns:
+        str: String containing "Hello world!".
+    """
+    return "Hello world!"
---
+++ test/test_hello_world.py
@@ -0,0 +1,4 @@
+from src.hello_world import say_hello
+
+def test_hello_world():
+    assert say_hello() == "Hello world!"
