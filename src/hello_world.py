3bca551dfe3bfc542848ccd10e2ab7d5e1784573
Joachim Holwech <joachim.holwech@gmail.com>
2021-12-01 16:24:28 +0100

    Formatting

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,6 +1,7 @@
 """This file is an example containing a docstring.
 """
 
+
 def say_hello():
     """This function return a string containing "Hello world!".
 
--- test/test_hello_world.py
+++ test/test_hello_world.py
@@ -1,4 +1,5 @@
 from src.hello_world import say_hello
 
+
 def test_hello_world():
     assert say_hello() == "Hello world!"
